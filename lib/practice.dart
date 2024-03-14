a3a49ab8bdee4743fc5a75b8b2a7ba3308ad89de
DevNadimm <nadimchowdhury87@gmail.com>
2024-03-14 21:29:42 +0600

    some update

--- lib/practice.dart
+++ lib/practice.dart
@@ -1,5 +1,20 @@
 void main(){
-  var name = 'Nadim';
-  print('Hello World');
+
+  int num1 = 20;
+  double num2 = 3.1416;
+  int result = num1+num2.toInt();
+  print(result);
+
+  double result2 = num1+num2;
+  print(result2);
+
+  print(result2.runtimeType) ;
+  print(result2.toStringAsFixed(1));
+
+  String name = result2.toString();
   print(name);
+  print(result2.runtimeType);
+
+  int myRess = result2.toInt() + 200;
+
 }
