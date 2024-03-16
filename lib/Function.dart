7ebda64de9124d36608998fa5dbe5dd41e95ffaf
DevNadimm <nadimchowdhury87@gmail.com>
2024-03-16 22:47:40 +0600

    Updated

--- lib/Function.dart
+++ lib/Function.dart
@@ -1,8 +1,20 @@
-void main(){
-  add(10,11);
-  }
-double add(int a, double b) {
-  double result = a + b;
-  print(result);
-  return result;
+// void main(){
+//   add(12, 10);
+//   }
+// double add(int a, double b) {
+//   double result = a + b;
+//   print(result);
+//   return result;
+// }
+
+
+void main (){
+  sayBot (name: 'Nadim',days: 20);
+  sayBot (name: 'Rafat',days: 20);
+
+}
+
+void sayBot ({required String name, int ? days}){
+  print('Hey $name Good Morning!');
+  print('Welcome to day $days.' );
 }
