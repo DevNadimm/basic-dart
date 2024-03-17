82bc6b5989a865ad62d8c4bc6bc9522439ea8727
DevNadimm <nadimchowdhury87@gmail.com>
2024-03-17 17:16:53 +0600

    Updated

--- lib/Function.dart
+++ lib/Function.dart
@@ -8,12 +8,29 @@
 // }
 
 
-void main (){
-  sayBot (name: 'Nadim',days: 20);
-  sayBot (name: 'Rafat',days: 30);
-}
-void sayBot ({required String name, int? days }){
-  print('Hey $name Good Morning!');
-  print('Welcome to day $days.' );
-}
+// void main (){
+//   sayBot (name: 'Nadim',days: 20);
+//   sayBot (name: 'Rafat',days: 30);
+// }
+// void sayBot ({required String name, int? days }){
+//   print('Hey $name Good Morning!');
+//   print('Welcome to day $days.' );
+// }
 
+
+void main (){
+  salam (name: 'Nadim',project: 2024,sirName: 'Rifat');
+  salam (name: 'Fatiha',project: 2023,sirName: 'Fatima');
+  salam (name: 'Rafat',project: 2024,sirName: 'Rifat');
+}
+void salam ({String? name, int? project, String? sirName}){
+
+  print("Assalamualaikum $name sir! Good Morning");
+
+  if (project != null){
+    print("Where is your project $project");
+  } else {
+    print("Project is not specified");
+  }
+  print("Apnk $sirName sir dakce");
+}
