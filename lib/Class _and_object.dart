294fb6fb249a289de1742a6f5f960f9bd66a898f
DevNadimm <nadimchowdhury87@gmail.com>
2024-03-20 11:07:12 +0600

    Updated

--- lib/Class _and_object.dart
+++ lib/Class _and_object.dart
@@ -1,14 +1,28 @@
-class myClass{
-int a=10;
-int b=11;
+/*
+class myClass {
+  int a = 10;
+  int b = 11;
+  int sum = 0;
 
+  myClass() {
+    sum = a + b;
+  }
+}
+
+void main() {
+  var classObj = myClass();
+  print(classObj.sum);
+}
+*/
+
+class myClass{
+  int a=10;
+  int b=11;
 }
 
 void main (){
   var classObj = myClass();
   int toeSum =(classObj.a+classObj.b);
   print(toeSum);
-
-
 }
 
