f5a4ac5280332ca3cbf74e3c14752eb3015c9b94
DevNadimm <nadimchowdhury87@gmail.com>
2024-03-20 12:00:07 +0600

    Updated

--- lib/null-aware coalescing operator.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  int? nullableValue; // A nullable integer
-
-  // Using the null-aware coalescing operator to provide a default value
-  int nonNullableValue = nullableValue ?? 42;
-
-  print("Value (non-nullable): $nonNullableValue");
-}
